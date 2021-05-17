6de4620a78a03c9a026223e174326b7016caef65
19044628 <19044628@rp.edu.sg>
2021-05-17 16:34:36 +0800

    Add project files.

---
+++ testFYP/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace testFYP.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public int StockAvailable { get; set; }
+        //Image  ProductPicture  { get; set; }
+        public int CategoryId { get; set; }
+
+}
+}
